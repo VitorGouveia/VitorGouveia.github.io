9cb043d917a145c758c8e64120749876b56dd649
VitorGouveia <vitorneves.gouveia10@gmail.com>
2023-02-02 19:54:50 -0300

    use old portifolio

---
+++ src/deps.js
@@ -0,0 +1,12 @@
+// @TODO: make logic to lazy load deps
+export { html, render } from "lit";
+
+export {
+  virtual,
+  component,
+  useState,
+  useCallback,
+  useRef,
+  useEffect,
+  useLayoutEffect,
+} from "haunted";
