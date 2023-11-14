f68b070f6cd7bfd1d1ddbff600094bbc2dbaf2e5
VitorGouveia <vitorneves.gouveia10@gmail.com>
2023-11-14 16:07:17 -0300

    hero

---
+++ prettier.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss")
+  ],
+  pluginSearchDirs: false,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro"
+      }
+    }
+  ],
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "es5",
+  semi: false,
+  bracketSameLine: false,
+  tailwindConfig: "./tailwind.config.cjs"
+}
